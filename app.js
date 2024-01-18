b8875d848ccaf69ff79a4e06218d44b9dff40184
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2024-01-18 19:17:27 +1100

    disabled app.js

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-const express = require('express');
-const axios = require('axios');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const axios = require('axios');
+// const bodyParser = require('body-parser');
 
-const expressApp = express();
-const port = 3000;
+// const expressApp = express();
+// const port = 3000;
 
-// Middleware to parse JSON bodies
-expressApp.use(bodyParser.json());
+// // Middleware to parse JSON bodies
+// expressApp.use(bodyParser.json());
 
-expressApp.get('/', (req, res) => {
-  res.sendFile(__dirname +'/index.html');
-});
+// expressApp.get('/', (req, res) => {
+//   res.sendFile(__dirname +'/index.html');
+// });
 
-expressApp.use(express.static('public'));
+// expressApp.use(express.static('public'));
 
 
-// Endpoint to handle the request
-expressApp.post('/send-request', async (req, res) => {
-  try {
-    // Destructure the data from the request body
-    const { method, url, headers, body } = req.body;
+// // Endpoint to handle the request
+// expressApp.post('/send-request', async (req, res) => {
+//   try {
+//     // Destructure the data from the request body
+//     const { method, url, headers, body } = req.body;
 
-    // Send the request using Axios
-    const response = await axios({
-      method: method,
-      url: url,
-      headers: headers,
-      data: body
-    });
+//     // Send the request using Axios
+//     const response = await axios({
+//       method: method,
+//       url: url,
+//       headers: headers,
+//       data: body
+//     });
 
-    // Send the response back to the client
-    res.json({
-      status: response.status,
-      headers: response.headers,
-      data: response.data
-    });
-  } catch (error) {
-    // Handle errors
-    res.status(500).json({ message: error.message });
-  }
-});
+//     // Send the response back to the client
+//     res.json({
+//       status: response.status,
+//       headers: response.headers,
+//       data: response.data
+//     });
+//   } catch (error) {
+//     // Handle errors
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
-expressApp.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
-});
+// expressApp.listen(port, () => {
+//   console.log(`Server listening at http://localhost:${port}`);
+// });
 
-module.exports = expressApp; 
+// module.exports = expressApp; 
